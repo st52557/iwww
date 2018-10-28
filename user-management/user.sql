d1e9e956749b8f48162359f0c4e08063153ce224
st52557 <43813109+st52557@users.noreply.github.com>
2018-10-28 11:05:48 +0100

    Update user.sql

--- user-management/user.sql
+++ user-management/user.sql
@@ -2,6 +2,7 @@ create table user (
   id int AUTO_INCREMENT   primary key,
   username   varchar(63)  not null,
   password   varchar(63)  not null,
-  email      varchar(127) not null,
+  email      varchar(63) not null,
+  desctription varchar(127),
   created   datetime      not null
 );
